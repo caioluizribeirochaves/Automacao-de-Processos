3555272e17aa3d5a9ecb906a324782044d839b03
Caio Luiz <caioriberio@gmail.com>
2026-06-02 10:09:16 -0300

    Update Automacao_codigo.py

--- Automacao_codigo.py
+++ Automacao_codigo.py
@@ -105,16 +105,105 @@ meta_ticketmedio_ano = 500
 
 outlook = win32.Dispatch('outlook.application')
 
-nome = emails.loc[emails['Loja']==loja, 'E-mail'].values[0]
+nome = emails.loc[emails['Loja']==loja, 'Gerente'].values[0]
 mail = outlook.CreateItem(0)
 mail.To = emails.loc[emails['Loja']==loja, 'E-mail'].values[0]
 mail.Subject = f'OnePage Dia {dia_indicador.day}/{dia_indicador.month} - Loja {loja}'
-mail.Body = ''
-# Ou mail.HTMLBody = <p></p>
 
+if faturamento_dia >= meta_faturamento_dia:
+    cor_fat_dia = 'green'
+else:
+    cor_fat_dia = 'red'
+
+if faturamento_ano >= meta_faturamento_ano:
+    cor_fat_ano = 'green'
+else:
+    cor_fat_ano = 'red'
+
+if quantidade_prod_dia >= meta_qtdeprodutos_dia:
+    cor_qtde_dia = 'green'
+else:
+    cor_qtde_dia = 'red'
+
+if quantidade_prod_ano >= meta_qtdeprodutos_ano:
+    cor_qtde_ano = 'green'
+else:
+    cor_qtde_ano = 'red'
+
+if ticket_medio_dia >= meta_ticketmedio_dia:
+    cor_ticket_dia = 'green'
+else:
+    cor_ticket_dia = 'red'
+
+if ticket_medio_ano >= meta_ticketmedio_ano:
+    cor_ticket_ano = 'green'
+else:
+    cor_ticket_ano = 'red'
+
+mail.HTMLBody = f'''
+<p>Olá, espero que esteja bem, {nome}</p>
+
+<p>O resultado do dia <strong>{dia_indicador.day}/{dia_indicador.month}</strong> da <strong>loja {loja}</strong> foi:</p>
+
+<table>
+    <tr>
+        <th>Indicador</th>
+        <th>Valor Dia</th>
+        <th>Meta Dia</th>
+        <th>Cenário Dia</th>
+    </tr>
+    <tr>
+        <td>Faturamento</td>
+        <td style="text-align: center;">R${faturamento_dia:.2f}</td>
+        <td style="text-align: center;">R${meta_faturamento_dia:.2f}</td>
+        <td style="text-align: center;"><font color="{cor_fat_dia}">◙</td>
+    </tr>
+    <tr>
+        <td>Diversidade de Produtos</td>
+        <td style="text-align: center;">{quantidade_prod_dia}</td>
+        <td style="text-align: center;">{meta_qtdeprodutos_dia}</td>
+        <td style="text-align: center;"><font color="{cor_qtde_dia}">◙</td>
+    </tr>
+    <tr>
+        <td>Ticket Médio</td>
+        <td style="text-align: center;">R${ticket_medio_dia:.2f}</td>
+        <td style="text-align: center;">R${meta_ticketmedio_dia:.2f}</td>
+        <td style="text-align: center;"><font color="{cor_ticket_dia}">◙</td>
+    </tr>
+</table>
+<br>
+<table>
+    <tr>
+        <th>Indicador</th>
+        <th>Valor Ano</th>
+        <th>Meta Ano</th>
+        <th>Cenário Ano</th>
+    </tr>
+    <tr>
+        <td>Faturamento</td>
+        <td style="text-align: center;">R${faturamento_ano:.2f}</td>
+        <td style="text-align: center;">R${meta_faturamento_ano:.2f}</td>
+        <td style="text-align: center;"><font color="{cor_fat_ano}">◙</td>
+    </tr>
+    <tr>
+        <td>Diversidade de Produtos</td>
+        <td style="text-align: center;">{quantidade_prod_ano}</td>
+        <td style="text-align: center;">{meta_qtdeprodutos_ano}</td>
+        <td style="text-align: center;"><font color="{cor_qtde_ano}">◙</td>
+    </tr>
+    <tr>
+        <td>Ticket Médio</td>
+        <td style="text-align: center;">R${ticket_medio_ano:.2f}</td>
+        <td style="text-align: center;">R${meta_ticketmedio_ano:.2f}</td>
+        <td style="text-align: center;"><font color="{cor_ticket_ano}">◙</td>
+    </tr>
+</table>
+
+<p>Segue em anexo a planilha com todos os dados para mais detalhes.</p>
+<p>Att., Caio Luiz.</p>
+'''
 # Anexos
 attachment = pathlib.Path.cwd() / caminho_backup / loja / f'{dia_indicador.day}_{dia_indicador.month}_{loja}.xlsx'
 mail.Attachments.Add(str(attachment))
 
 mail.Send()
-
